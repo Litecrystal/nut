fea8f6a735a9157ed48e51ea3ad4e069e9e345d9
Litecrystal <70708321+Litecrystal@users.noreply.github.com>
2020-09-27 16:19:57 +0200

    Add files via upload

---
+++ Nut/script.js
@@ -0,0 +1,41 @@
+var nuts
+var nut, nut2, nut3
+var audio
+var score 
+var overnut
+
+function preload() {
+ nut = loadImage("assets/nut.png")
+ nut3 = loadImage("assets/nut3.png")
+}
+
+
+function setup() {
+ createCanvas(800,700)
+ audio = loadSound("assets/nutmp3.mp3");
+ score = 0
+ overnut = "Nuts:"
+ nut.loadPixels()
+ nut3.loadPixels()
+ nuts = []
+ nuts.push(nut)
+ nuts.push(nut2)
+
+
+}
+
+
+function draw() {
+  background(255, 128, 0)
+  image(nut3, 250, 200, 300, 300)
+  textSize(35)
+  text(score, 190, 31)
+  text(overnut, 100, 30)
+
+}
+
+
+function mousePressed() {
+  audio.play();
+  score = score + 1
+}
