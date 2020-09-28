252d8d7700520f4d32e55f275ecbf62154e7d3ab
Litecrystal <70708321+Litecrystal@users.noreply.github.com>
2020-09-28 14:43:02 +0200

    Add files via upload

--- Nut/script.js
+++ Nut/script.js
@@ -4,6 +4,7 @@ var audio
 var score 
 var overnut
 
+
 function preload() {
  nut = loadImage("assets/nut.png")
  nut3 = loadImage("assets/nut3.png")
@@ -13,7 +14,7 @@ function preload() {
 function setup() {
  createCanvas(800,700)
  audio = loadSound("assets/nutmp3.mp3");
- score = 0
+ score = document.cookie;
  overnut = "Nuts:"
  nut.loadPixels()
  nut3.loadPixels()
@@ -38,4 +39,38 @@ function draw() {
 function mousePressed() {
   audio.play();
   score = score + 1
+}
+
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user=getCookie("username");
+  if (user != "") {
+  } else {
+     user = prompt("Choose a name if you want your progress to be saved:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  }
 }
