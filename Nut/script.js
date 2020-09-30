7b01b813c4015018b62f5227008a216adf02c9b9
Litecrystal <70708321+Litecrystal@users.noreply.github.com>
2020-09-30 16:55:30 +0200

    Add files via upload

--- Nut/script.js
+++ Nut/script.js
@@ -3,6 +3,7 @@ var nut, nut2, nut3
 var audio
 var score 
 var overnut
+var discord 
 
 
 function preload() {
@@ -12,7 +13,7 @@ function preload() {
 
 
 function setup() {
- createCanvas(1436,707)
+ createCanvas(1920,1080)
  audio = loadSound("assets/nutmp3.mp3");
  score = 0
  overnut = "Nuts:"
@@ -21,6 +22,7 @@ function setup() {
  nuts = []
  nuts.push(nut)
  nuts.push(nut2)
+ discord = "Discord:https://discord.gg/szGRfru"
 
 
 }
@@ -28,10 +30,11 @@ function setup() {
 
 function draw() {
   background(255, 128, 0)
-  image(nut3, 500, 500, 300, 300)
+  image(nut3, 900, 500, 300, 300)
   textSize(35)
   text(score, 190, 31)
   text(overnut, 100, 30)
+  text(discord, 1425, 973)
 
 }
 
